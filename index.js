e29adb953fe997bbecede3c671d8d9966d175f4c
idris <idreesnoor150@gmail.com>
2023-03-22 13:45:18 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here!const testVar = "h1";
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Victor is the champion";
+newHeader.className = "victory";
